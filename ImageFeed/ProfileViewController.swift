ae20687bf69e90a0b0651fca99de00fef2f51fc7
Moxa <gainitfuture@gmail.com>
2026-03-05 22:44:54 +0800

    Коммит перед версткой кодом

---
+++ ImageFeed/ProfileViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Moxa on 18/02/26.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    
+    @IBOutlet weak var logo: UIImageView!
+    
+    @IBOutlet weak var exit: UIButton!
+    
+    @IBAction func exitAction(_ sender: Any) {
+        
+        
+    }
+    
+    
+}
+
